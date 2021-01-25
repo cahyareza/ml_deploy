5313321c3c5a3b4203a7318e372f00f35417ede9
cahyafaisalreza <cf.reza07@gmail.com>
2021-01-26 00:48:23 +0700

    edit settings.py with add logging

--- loannotloan/settings.py
+++ loannotloan/settings.py
@@ -133,3 +133,19 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
